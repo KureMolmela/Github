516be56263cd936c33c4198136b2c648e6fc99d1
KureMolmela <molmela.kure-yusuf@binghamuni.edu.ng>
2024-05-11 08:19:01 +0100

    implemented

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
 # guessing game
+
+from random import randint
+
+number = randint(1, 5)
+lives = 3
+
+while lives > 0:
+    choice = int(input("guess number: "))
+    if choice == number:
+        print("well done")
+        break
+    else:
+        lives = lives - 1
